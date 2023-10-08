bf22b076320425b60b0a8e2a66128f6c1a96da7a
Johnny <005330373@coyote.csusb.edu>
2023-10-07 21:28:34 -0700

    feat: add more fields to signup

--- src/api.js
+++ src/api.js
@@ -17,12 +17,28 @@ async function signInWithEmailAndPasswordLocally(email, password) {
   }
 }
 
-async function signUpWithEmailAndPasswordLocally(email, password) {
+async function signUpWithEmailAndPasswordLocally(
+  email,
+  password,
+  dob,
+  first_name,
+  last_name,
+  title,
+  role
+) {
   try {
     return await fetch(API_URL + "/api/auth/signup", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, password }),
+      body: JSON.stringify({
+        email,
+        password,
+        dob,
+        first_name,
+        last_name,
+        title,
+        role,
+      }),
     }).then((res) => res.json());
   } catch (err) {
     console.error(err);
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -3,20 +3,48 @@ import { useNavigate } from "react-router-dom";
 import { signUpWithEmailAndPasswordLocally } from "../api.js";
 
 function Signup() {
-  const [input, setInput] = useState({ email: "", password: "" });
+  const [input, setInput] = useState({
+    email: "",
+    password: "",
+    "bday-year": "",
+    "bday-month": "",
+    "bday-day": "",
+    first_name: "",
+    last_name: "",
+    title: "",
+    role: "PATIENT",
+  });
   const [error, setError] = useState(null);
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setError("");
-    let email = input.email.toLowerCase().trim();
-    let password = input.password;
+    const email = input.email.toLowerCase().trim();
+    const password = input.password;
+    const dob = `${input["bday-year"]}-${input["bday-month"]}-${input[
+      "bday-day"
+    ]
+      .toString()
+      .padStart(2, 0)}`;
+    const first_name = input.first_name;
+    const last_name = input.last_name;
+    const title = input.title;
+    const role = input.role;
+    console.log(input);
 
-    signUpWithEmailAndPasswordLocally(email, password)
+    signUpWithEmailAndPasswordLocally(
+      email,
+      password,
+      dob,
+      first_name,
+      last_name,
+      title,
+      role
+    )
       .then((data) => {
         if (data.errors) {
-          alert(data.errors[0].msg);
+          setError(data.errors[0].msg);
         } else {
           navigate("/dashboard");
         }
@@ -36,12 +64,104 @@ function Signup() {
       <form autoComplete="off" className="form" onSubmit={handleSubmit}>
         <h1>Sign Up</h1>
         <div className="form-row">
+          <input
+            name="first_name"
+            type="text"
+            onChange={handleChange}
+            value={input.first_name}
+            required
+            autoComplete="true"
+            placeholder="First Name"
+          />
+          <input
+            name="last_name"
+            type="text"
+            onChange={handleChange}
+            value={input.last_name}
+            required
+            autoComplete="true"
+            placeholder="Last Name"
+          />
+        </div>
+        <div className="form-row">
+          <select
+            className="form-select"
+            name="title"
+            onChange={handleChange}
+            value={input["title"]}
+          >
+            <option default hidden value="">
+              Title
+            </option>
+            <option value="Mr.">Mr.</option>
+            <option value="Mrs.">Mrs.</option>
+            <option value="Ms.">Ms.</option>
+            <option value="Dr.">Dr.</option>
+          </select>
+          <select
+            className="form-select"
+            name="title"
+            onChange={handleChange}
+            value={input["role"]}
+          >
+            <option value="PATIENT">Patient</option>
+            <option value="PHYSICIAN">Physician</option>
+            <option value="RADIOLOGIST">Radiologist</option>
+          </select>
+        </div>
+        <div className="form-row">
+          <div className="form-label">
+            <label>
+              <span>Date of Birth</span>
+              <select
+                autoComplete="bday-month"
+                className="form-select"
+                name="bday-month"
+                required
+                onChange={handleChange}
+                value={input["bday-month"]}
+              >
+                <option default disabled hidden value="">
+                  Month
+                </option>
+                <option value="01">January</option>
+                <option value="02">February</option>
+                <option value="03">March</option>
+                <option value="04">April</option>
+                <option value="05">May</option>
+                <option value="06">June</option>
+                <option value="07">July</option>
+                <option value="08">August</option>
+                <option value="09">September</option>
+                <option value="10">October</option>
+                <option value="11">November</option>
+                <option value="12">December</option>
+              </select>
+              <input
+                autoComplete="bday-day"
+                name="bday-day"
+                onChange={handleChange}
+                placeholder="Day"
+                type="number"
+              />
+              <input
+                autoComplete="bday-year"
+                name="bday-year"
+                onChange={handleChange}
+                placeholder="Year"
+                type="string"
+              />
+            </label>
+          </div>
+        </div>
+        <div className="form-row">
           <div className="form-label">
             <label htmlFor="email">
               <span>Email</span>
             </label>
           </div>
           <input
+            id="email"
             name="email"
             type="text"
             onChange={handleChange}
@@ -57,18 +177,19 @@ function Signup() {
             </label>
           </div>
           <input
+            id="password"
             name="password"
+            type="password"
             onChange={handleChange}
             value={input.password}
-            type="password"
             required
             autoComplete="true"
           />
         </div>
         <div className="form-row">
           <div className="btn">
-            {error ? <p>{error}</p> : null}
-            <button title="Signin" aria-label="Signin" type="submit">
+            {error ? <p style={{ color: "red" }}>{error}</p> : null}
+            <button title="Signup" aria-label="Signup" type="submit">
               Sign Up
             </button>
           </div>
